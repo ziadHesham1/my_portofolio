7b8578489b58a26fc85386296119ee1a4fd7fc55
Ziad Hesham <ziadhesham280@gmail.com>
2025-04-20 12:11:43 +0200

    feat: add project details localization
    This commit adds localization support for the project details page title.
    
    Key changes:
    
    - Added the `project_details` key to the English and Arabic translation files.
    - Updated the `ProjectDetailsPage` to use the translated string for the app bar title.

--- lib/features/project_details/presentation/project_details_page.dart
+++ lib/features/project_details/presentation/project_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:ziad_dev/common/style/portfolio_colors.dart';
 
@@ -9,7 +10,8 @@ class ProjectDetailsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.primary,
       appBar: AppBar(
-        title: const Text('Project Details'),
+        backgroundColor: AppColors.primary,
+        title: Text("project_details".tr()),
       ),
       body: const Center(
         child: Text(''),
